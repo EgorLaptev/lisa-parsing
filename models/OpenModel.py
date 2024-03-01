9b56583a744b7211746a538c770ba53052441ce0
MrL013 <lev.rock@yandex.ru>
2024-03-01 22:40:36 +0300

    update openmodel

--- models/OpenModel.py
+++ models/OpenModel.py
@@ -1,99 +1,3 @@
-# from typing import List, Dict
-
-# from openai import OpenAI
-# from dotenv import load_dotenv
-# from os import getenv
-# import json
-
-# from helpers.DocumentFormatter import DocumentFormatter
-# from datetime import datetime
-# from termcolor import colored
-
-
-# # Load model's role
-# load_dotenv('config/open.env')
-
-# with open('config/roles/role.txt', 'r', encoding='UTF-8') as file:
-#     role = file.read()
-
-
-# class OpenModel:
-#     def __init__(self):
-#         self.logging = False
-#         self.model = getenv('MODEL')
-#         self.token = getenv('AUTH_TOKEN')
-#         self.temperature = float(getenv('TEMPERATURE'))
-#         self.max_tokens = int(getenv('MAX_TOKENS'))
-#         self.top_p = float(getenv('TOP_P'))
-
-#         self.client = OpenAI(api_key=self.token)
-
-#         self.messages = []
-
-#         # set role
-#         self.messages.append({
-#             'role': 'system',
-#             'content': role
-#         })
-
-#     def send(self, content: str) -> str:
-#         """ sending  a message to the model and getting response """
-#         message = {'role': 'user', 'content': content}
-
-#         response = self.client.chat.completions.create(
-#             model=self.model,
-#             messages=self.messages + [message],
-#             temperature=self.temperature
-#         )
-
-#         response_content = response.choices[0].message.content
-
-#         if self.logging:
-#             OpenModel._log(response_content)
-
-#         return response_content
-
-#     def send_file(self, path: str):
-#         """ structures the file in json """
-#         chunks = DocumentFormatter.split(path)
-        
-#         responses = []
-            
-#         for i in range(len(chunks)):
-#             chunk = chunks[i]
-#             correctJSON = False
-#             while not correctJSON:
-#                 count_bags = 0
-#                 print(colored(f"[{i+1} File] Attemping..", 'dark_grey'))
-#                 try: 
-#                     resp = self.send(chunk.page_content)
-#                     json.loads(resp) # validate JSON
-#                     responses.append(resp)
-#                     OpenModel._log(resp)
-#                     correctJSON = True
-#                     print(colored(f"[{i+1} File] Success", 'light_green'))
-#                 except:
-#                     correctJSON = False
-#                     print(colored(f"[{i+1} File] Corrupted JSON", 'red'))
-#                     count_bags += 1
-#                 if count_bags > 10:
-#                     break
-
-#         # save results
-#         current_datetime = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-
-#         with open(f'data/{current_datetime}.json', 'w', encoding='UTF-8') as file_json:
-#             data = DocumentFormatter.merge_json(responses)
-#             json.dump(data, file_json, ensure_ascii=False)
-
-#     @staticmethod
-#     def _log(content):
-#         """ saves the model's responses """
-#         current_datetime = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-        
-#         with open(f'logs/log_{current_datetime}.txt', 'w', encoding='UTF-8') as log:
-#             log.write(content)
-
 
 from typing import List, Dict
 from openai import OpenAI
@@ -192,7 +96,7 @@ class OpenModel:
     
     @staticmethod
     def _deep_merge_json(json_list):
-        """Глубокое слияние списка JSON-объектов"""
+        """deep merge list json objects"""
         merged = {}
 
         for json_obj in json_list:
@@ -202,7 +106,7 @@ class OpenModel:
 
     @staticmethod
     def _merge_json(json1, json2):
-        """Глубокое слияние двух JSON-объектов"""
+        """deep merge two json objects"""
         merged = json1.copy()
 
         for key, value in json2.items():
