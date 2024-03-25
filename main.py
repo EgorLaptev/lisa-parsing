555847474975f301223c58fb910bf0f72552bf6f
EgorLaptev <egor.lapteffff@gmail.com>
2024-03-25 19:55:13 +0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# from models.OpenModel import OpenModel
+from models.OpenModel import OpenModel
 # from models.GigaModel import GigaModel
-from models.GeminiModel import GeminiModel
+# from models.GeminiModel import GeminiModel
 
-model = GeminiModel()
+model = OpenModel()
 
 model.send_file('docs/txt/ГОСТ_7_32_2017.txt')
 
